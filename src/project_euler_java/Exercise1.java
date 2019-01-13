9be66f2067cfef255f9800c507be664d00d9d789
Maciej Kawka <maciej.kawka@objective-partner.com>
2019-01-13 17:02:06 +0100

    removed comments and added mock implementation

--- src/project_euler_java/Exercise1.java
+++ src/project_euler_java/Exercise1.java
@@ -1,16 +1,11 @@
-/* Copyright 2019 objective partner AG, all rights reserved */
 package project_euler_java;
 
-/**
- * @author mkawka
- *
- */
 public class Exercise1 implements Solvable {
 
   
   @Override
   public void solve() {
-    // TODO Auto-generated method stub
+   System.out.println("Maciek jest slodziutki");
     
   }
 
--- src/project_euler_java/ProjectEuler.java
+++ src/project_euler_java/ProjectEuler.java
@@ -1,18 +1,13 @@
-/* Copyright 2019 objective partner AG, all rights reserved */
 package project_euler_java;
 
-/**
- * @author mkawka
- *
- */
+
 public class ProjectEuler {
 
-  /**
-   * @param args
-   */
+  
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
-
+    Exercise1 solution = new Exercise1();
+    solution.solve();
+    
   }
 
 }
