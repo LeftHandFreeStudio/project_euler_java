851eb7c0c95cbe9e4b5b660772b1150ce878c961
Maciej Kawka <maciej.kawka@objective-partner.com>
2019-01-20 10:10:14 +0100

    solution of Exercise 2 added

--- src/project_euler_java/ProjectEuler.java
+++ src/project_euler_java/ProjectEuler.java
@@ -7,6 +7,7 @@ public class ProjectEuler {
   public static void main(String[] args) {
     Solvable[] solutions = new Solvable[641];
     solutions[0]  = new Exercise1();
+    solutions[1] = new Exercise2();
     for (Solvable solvable : solutions) {
       if (solvable != null) {
         solvable.solve();
